5f3685430ad5cb59cbaf52346d29e21669f15304
klemonade <51604717+klemonade@users.noreply.github.com>
2019-11-04 23:33:32 +0700

    Add files via upload

--- fix_arithmetic_sequence.c
+++ fix_arithmetic_sequence.c
@@ -5,7 +5,7 @@ int main()
     scanf("%d",&n);
     int a[n];
     int df[n];
-    int i;
+    int i,j;
     for (i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
@@ -14,26 +14,22 @@ int main()
     {
         df[i]=a[i+1]-a[i];
     }
-    int rdif;
+    int rdif=df[0];
     for (i=0;i<n-2;i++)
     {
-        if (df[i+1]!=df[i])
+        if (rdif > df[i])
         {
-            if(df[i+1]!=df[i+2])
-            {
-                rdif = df[i]; 
-            }
-            else 
-            {
-                rdif = df[i+1];
-            }
+            rdif = df[i];
         }
     }
     for (i=0;i<n-1;i++)
     {
         if (a[i+1]-a[i]!=rdif)
         {
-            printf("%d",a[i]+rdif);
+            for (j=1;j<a[i+1]-a[i]/rdif;j++)
+            {
+                printf("%d ",a[i]+j*rdif);
+            }
         }
     }
 }
